549bad199c828812ac26390b6fc97025e46d9789
NlodeL <nlodelop@myuax.com>
2024-04-23 11:08:16 +0200

    compruebo que todo lo relacionado con transacciones funcione y añado un comentario para explicar el formato de la feccha

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import Interfaz.InterfazConBotones;
 
 import javax.swing.*;
-
+//formato fecha para añadir transacciones YYYY-MM-DD
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
