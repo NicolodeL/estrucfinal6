229c3dc251d49b1e309d62ee10134c442ad9cf55
NlodeL <nlodelop@myuax.com>
2024-04-23 09:00:54 +0200

    rehago la clase pareja

--- src/main/java/DatosDinamicos/Pareja.java
+++ src/main/java/DatosDinamicos/Pareja.java
@@ -1,4 +1,19 @@
 package DatosDinamicos;
 
 public class Pareja {
+    private int first;
+    private int second;
+
+    public Pareja(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public int getSecond() {
+        return second;
+    }
 }
